61027de8ae11c1b46ee153960f273651ccfea997
Hans Höglund <hans@hanshoglund.se>
2013-06-15 02:16:01 +0200

    New quantization test

--- src/Music/Score/Rhythm.hs
+++ src/Music/Score/Rhythm.hs
@@ -98,10 +98,10 @@ quantize :: Tiable a => [(DurationT, a)] -> Either String (Rhythm a)
 quantize = quantize' (atEnd rhythm)
 
 
--- Internal...
 
-testQuantize :: [DurationT] -> Either String (Rhythm ())
-testQuantize = quantize' (atEnd rhythm) . fmap (\x->(x,()))
+
+
+-- Internal...
 
 dotMod :: Int -> DurationT
 dotMod n = dotMods !! (n-1)
@@ -114,7 +114,6 @@ dotMods = zipWith (/) (fmap pred $ drop 2 times2) (drop 1 times2)
 
 tupletMods :: [DurationT]
 tupletMods = [2/3, 4/5, {-4/6,-} 4/7, 8/9]
-
 
 -- 3/2 for dots
 -- 2/3, 4/5, 4/6, 4/7, 8/9, 8/10, 8/11  for ordinary tuplets
@@ -146,13 +145,11 @@ type RhythmParser a b = Parsec [(DurationT, a)] RState b
 quantize' :: Tiable a => RhythmParser a b -> [(DurationT, a)] -> Either String b
 quantize' p = left show . runParser p mempty ""
 
--- Matches a (duration, value) pair iff the predicate matches, returns beat
-match :: Tiable a => (DurationT -> a -> Bool) -> RhythmParser a (Rhythm a)
-match p = tokenPrim show next test
-    where
-        show x        = ""
-        next pos _ _  = updatePosChar pos 'x'
-        test (d,x)    = if p d x then Just (Beat d x) else Nothing
+testQuantize :: RhythmParser () b -> [DurationT] -> Either String b
+testQuantize p = quantize' (atEnd p) . fmap (\x->(x,()))
+
+
+
 
 -- Matches any rhythm
 rhythm :: Tiable a => RhythmParser a (Rhythm a)
@@ -165,9 +162,71 @@ rhythm' :: Tiable a => RhythmParser a (Rhythm a)
 rhythm' = mzero
     <|> beat
     <|> dotted
-    <|> tuplet
+    <|> tuplet rhythmNoBound
 
--- Matches a beat divisible by 2 (notated)
+
+
+
+
+
+
+
+
+-- Matches a 2-based rhytm group (such as a 4/4 or 2/4 bar)
+rhythm2 :: Tiable a => RhythmParser a (Rhythm a)
+rhythm2 = mzero
+    <|> dur 1              
+    <|> (group $ fmap (scale $ 1/2) $ [rhythm2, rhythm2])
+    -- <|> try (seq2 rhythm2 rhythm2)
+    -- <|> try (seq2 rhythm2 rhythm3)
+    -- <|> try (rhythm2 >> rhythm2 >> rhythm2) -- syncopation etc
+    <|> (tuplet rhythm2)                   -- fixme should recur on 2 or 3
+
+-- Matches a 2-based rhytm group (such as a 3/4 or 3/8 bar)
+rhythm3 :: Tiable a => RhythmParser a (Rhythm a)
+rhythm3 = mzero
+    <|> dur 1.5
+    -- <|> try (seq2 rhythm2 rhythm2)         -- long-short or short-long
+    -- <|> try (seq3 rhythm3 rhythm3 rhythm3) -- for 9/8
+    -- <|> try (seq3 rhythm2 rhythm2 rhythm2) -- hemiola
+    -- <|> (tuplet rhythm2)                   -- fixme should recur on 2 or 3
+
+-- seq2 p q = do
+--     a <- p
+--     b <- q
+--     return $ Group [a,b]
+-- seq3 p q x = do
+--     a <- p
+--     b <- q
+--     c <- x
+--     return $ Group [a,b,c]
+group :: [RhythmParser a (Rhythm a)] -> RhythmParser a (Rhythm a)
+group ps = do
+    as <- Prelude.sequence ps
+    return $ Group as
+
+scale :: DurationT -> RhythmParser a (Rhythm a) -> RhythmParser a (Rhythm a)
+scale d p = do
+    modifyState $ modifyTimeMod (* d)
+    a <- p
+    modifyState $ modifyTimeMod (/ d)
+    return a
+
+
+
+
+-- Note: notatedDur == (actualDur / tm - ts))
+
+-- Matches exactly the given duration (modified by context).
+dur :: Tiable a => DurationT -> RhythmParser a (Rhythm a)
+dur d' = do
+    RState tm ts _ <- getState
+    (\d -> (d^/tm) `subDur` ts) <$> match (\d _ ->
+        d - ts > 0
+        &&
+        d' == (d / tm - ts))
+
+-- Matches a beat divisible by 2 (modified by context)
 beat :: Tiable a => RhythmParser a (Rhythm a)
 beat = do
     RState tm ts _ <- getState
@@ -192,7 +251,6 @@ dotted' n = do
 bound :: Tiable a => RhythmParser a (Rhythm a)
 bound = bound' (1/2)
 
-
 bound' :: Tiable a => DurationT -> RhythmParser a (Rhythm a)
 bound' d = do
     modifyState $ modifyTimeSub (+ d)
@@ -202,24 +260,33 @@ bound' d = do
     return $ Group [Beat (getBeatDuration a) $ b, Beat (1/2) $ c]
     -- FIXME doesn't know order
 
--- | Matches a tuplet
-tuplet :: Tiable a => RhythmParser a (Rhythm a)
-tuplet = msum . fmap tuplet' $ tupletMods
+-- | Matches a tuplet, recurring on the given parser.
+tuplet :: Tiable a => RhythmParser a (Rhythm a) -> RhythmParser a (Rhythm a)
+tuplet rec = msum . fmap (tuplet' rec) $ tupletMods
 
 -- tuplet' 2/3 for triplet, 4/5 for quintuplet etc
-tuplet' :: Tiable a => DurationT -> RhythmParser a (Rhythm a)
-tuplet' d = do
+tuplet' :: Tiable a => RhythmParser a (Rhythm a) -> DurationT -> RhythmParser a (Rhythm a)
+tuplet' rec d = do
     RState _ _ depth <- getState
     onlyIf (depth < 1) $ do                         -- max 1 nested tuplets
         modifyState $ modifyTimeMod (* d)
                     . modifyTupleDepth succ
-        a <- rhythmNoBound
+        a <- rec
         modifyState $ modifyTimeMod (/ d)
                     . modifyTupleDepth pred
         return (Tuplet d a)
 
 
 -------------------------------------------------------------------------------------
+
+-- Matches a (duration, value) pair iff the predicate matches, returns beat
+match :: Tiable a => (DurationT -> a -> Bool) -> RhythmParser a (Rhythm a)
+match p = tokenPrim show next test
+    where
+        show x        = ""
+        next pos _ _  = updatePosChar pos 'x'
+        test (d,x)    = if p d x then Just (Beat d x) else Nothing
+
 
 -- | Similar to 'many1', but tries longer sequences before trying one.
 many1long :: Stream s m t => ParsecT s u m a -> ParsecT s u m [a]
